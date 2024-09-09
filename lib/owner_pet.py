d29dfc2ff881130694a35ef428764d9abe2e0b80
Ngecha <ngechadennis@gmail.com>
2024-09-09 13:22:52 +0300

    commit

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,36 @@
 class Pet:
-    pass
-
+    
+    PET_TYPES=["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all=[]
+    
+    def __init__(self,name,pet_type,owner=None):
+        self.name=name
+        
+        if pet_type not in Pet.PET_TYPES:
+         raise Exception("Pet not on the list")
+        
+        self.pet_type=pet_type
+        self.owner=owner
+        self.add_to_All()
+        
+    def  add_to_All(name):
+        if name not in Pet.all:
+            Pet.all.append(name)
+    
+    
 class Owner:
-    pass
+    def __init__(self,name):
+        self.name=name
+        
+    
+    def pets(self):
+        return[pet for pet in Pet.all if pet.owner == self]
+    
+        
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Can only add instances of the Pet class.")
+        pet.owner = self   
+        
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
